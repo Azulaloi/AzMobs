185b55ef646d49307997ff8f977d27858cefe6df
Azulaloi <azulaloichampagne@gmail.com>
2017-08-19 18:33:02 -0400

    Gilded equipment spawning

--- src/main/java/az/azmobs/entity/EntityGilded.java
+++ src/main/java/az/azmobs/entity/EntityGilded.java
@@ -2,7 +2,11 @@ package az.azmobs.entity;
 
 import az.azmobs.AzMobs;
 import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 /**
@@ -11,6 +15,15 @@ import net.minecraft.world.World;
 public class EntityGilded extends EntitySkeleton {
     public EntityGilded(World worldIn) {
         super(worldIn);
+    }
+
+    protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty){
+        super.setEquipmentBasedOnDifficulty(difficulty);
+        if (this.rand.nextInt(3) == 0) {
+            this.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(Items.GOLDEN_SWORD));
+        } else {
+            this.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, ItemStack.EMPTY);
+        }
     }
 
     @Override
